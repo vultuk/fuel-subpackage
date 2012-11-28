ad05bad4df1effcf7ebc364a71afd07297a7dcc4
Simon Skinner <simonskinner@dk101.net>
2012-11-28 19:09:20 +0000

    Fixed main config issue with subconfig file

--- classes/subconfig.php
+++ classes/subconfig.php
@@ -7,10 +7,18 @@ class Subconfig extends \Config
 	
 	public static function subload($file, $group = null, $reload = false, $overwrite = false)
 	{
-		list($package, $subpackage, $configFile) = explode("\\", $file);
+		if (count(explode("\\", $file)) > 2)
+		{
+			list($package, $subpackage, $configFile) = explode("\\", $file);
 		
-		$fullFile = PKGPATH.$package."/subpackage/".$subpackage."/config/".$configFile.".php";
-		
+			$fullFile = PKGPATH.$package."/subpackage/".$subpackage."/config/".$configFile.".php";
+
+		}
+		else
+		{
+			$fullFile = $file;
+		}
+				
 		return static::load($fullFile, $file, $reload, $overwrite);
 		
 	}
